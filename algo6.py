53178872bfce762aed03bd83e7fae6b6962cbc16
Mahidhar C <mahidhar.c@gmail.com>
2014-03-19 19:27:52 +0530

    algo 6 draft2

--- algo6.py
+++ algo6.py
@@ -1,5 +1,6 @@
 from numpy import *
 from algo5 import *
+from pprint import *
 
 class TCPstream:
   destinationIP = ""
@@ -21,20 +22,23 @@ def updateProbabilities(stream, bandCount, band, observableTypeIndex):
     avgCount = 0
     stream.trainWindowCount += bandCount
     for j in range(0,bandCount):
+      '''print observableTypeIndex, band[j][0]'''
       nelband = band[j][0]
       for k in range(1,nelband+1):
-        avgCount += stream.probabilityArray[observableTypeIndex][band[j][k]]
+        avgCount += stream.probabilityArray[observableTypeIndex][band[j][k]-2]
       avgCount += 1
       avgCount /= nelband
       for k in range(1,nelband+1):
-        stream.probabilityArray[observableTypeIndex][band[j][k]] = avgCount
+        stream.probabilityArray[observableTypeIndex][band[j][k]-2] = avgCount
       for k in range(1,nelband+1):
-        stream.probabilityArray[observableTypeIndex][band[j][k]] /= stream.trainWindowCount
+        stream.probabilityArray[observableTypeIndex][band[j][k]-2] /= stream.trainWindowCount
       avgCount = 0
     return stream
 
 stream = TCPstream(10,5);
-A = array([500, 291, 271, 36, 222, 111, 1211, 3, 1, 31, 2])
-band = determineOptimalBands(A,10,5)
-for i in range(1,6):
+A = array([500, 291, 271, 36, 222, 111, 1211, 3, 2, 31, 1])
+band = determineOptimalBands(A,10,5) +1
+for i in range(6):
   stream = updateProbabilities(stream, stream.bandCount, band, i)
+
+pprint(stream.probabilityArray1)
