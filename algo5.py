279bf869b16c6050076a1719303a9966478fae1a
Mahidhar C <mahidhar.c@gmail.com>
2014-03-19 16:36:35 +0530

    algo 5 done

---
+++ algo5.py
@@ -0,0 +1,47 @@
+from numpy import *
+
+def determineOptimalBands(arrayA,windowSize,bandCount):
+  jumpCount = zeros(windowSize)
+  sortedJumps = zeros(bandCount-1)
+  upper = 0
+  lower = 0
+  nelements = 0
+  indexArray = zeros(bandCount-1)
+  band = zeros(bandCount*windowSize).reshape(bandCount, windowSize) - 1
+  finalK = -1
+
+  sortedA = sorted(arrayA,reverse=True)
+  for i in range(windowSize-1):
+    jumpCount[i] = sortedA[i] - sortedA[i+1]
+  temp = sorted(jumpCount,reverse=True)
+  for i in range(bandCount-1):
+    sortedJumps[i] = temp[i]
+
+  jumpCountPos = argsort(jumpCount)
+  sortedJumpsPos = searchsorted(jumpCount[jumpCountPos], sortedJumps)
+  indexArray = jumpCountPos[sortedJumpsPos]
+
+  for i in range(bandCount-1):
+    upper = indexArray[i]
+    nelements = upper - lower + 1
+    k = 0
+    band[i][k] = nelements
+    temp = sortedA[lower:lower+nelements]
+    arrayAPos = argsort(arrayA)
+    tempPos = searchsorted(arrayA[arrayAPos],temp,sorter=None)
+    for x in arrayAPos[tempPos]:
+      band[i][k] = x
+      k += 1
+      finalK += 1
+    lower += nelements
+
+  arrayAPosReversed = arrayAPos[::-1]
+  k=0
+  for x in range(finalK+1,windowSize+1):
+    band[i+1][k] = arrayAPosReversed[x]
+    k += 1
+  print band
+
+
+A = array([500, 291, 271, 36, 222, 111, 1211, 3, 1, 31, 1])
+determineOptimalBands(A,10,5)
