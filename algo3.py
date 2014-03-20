4a242a85f0fc0e5bc0b1508a13dfc71b765dc3c4
Nitesh A Jain <niteshjain92@gmail.com>
2014-03-20 19:17:25 +0530

    Updated  algorithm 3 and 6

--- algo3.py
+++ algo3.py
@@ -1,12 +1,23 @@
 from algo6 import *
+class TCPstream:
+  destinationIP = ""
+  destinationPort = ""
+  windowSize = 0
+  bandCount = 0
+  errorProportion = 0.0
+  trainWindowCount = 0
+  packetCounter = zeros(6)
+  probabilityArray = array([])
+  thresholdProbability = 0.0
+
+  def __init__(self,winSize,bandC):
+    self.windowSize = winSize
+    self.probabilityArray = zeros(self.windowSize*6).reshape(6,self.windowSize)
+    self.bandCount = bandC
+
 def TCPtrain(stream,trafficFlow,sizeW,bandC):
-    #TODO : figure out how to populate stream.c[i]
+    fp=fromfile("op_flag", dtype=int32, sep='\n' )
     for i in range(6):
         band=determineOptimalBands(stream.probabilityArray[i],sizeW,bandC)
         stream=updateProbabilities(stream,bandC,bandC,i)
-
-
-
-
-
-
+    return stream
--- algo6.py
+++ algo6.py
@@ -1,22 +1,5 @@
-from numpy import *
 from algo5 import *
-from pprint import *
 
-class TCPstream:
-  destinationIP = ""
-  destinationPort = ""
-  windowSize = 0
-  bandCount = 0
-  errorProportion = 0.0
-  trainWindowCount = 0
-  packetCounter = zeros(6)
-  probabilityArray = array([])
-  thresholdProbability = 0.0
-
-  def __init__(self,winSize,bandC):
-    self.windowSize = winSize
-    self.probabilityArray = zeros(self.windowSize*6).reshape(6,self.windowSize)
-    self.bandCount = bandC
 
 def updateProbabilities(stream, bandCount, band, observableTypeIndex):
     avgCount = 0
