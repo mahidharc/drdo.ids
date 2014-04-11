4481f1693cad140f6be5d520b2c3f5cfaccf8e7d
mahidhar <mahidhar.c@gmail.com>
2014-04-11 14:33:11 +0530

    algo 5 corrected

--- algo3.py
+++ algo3.py
@@ -19,7 +19,7 @@ class TCPstream:
 
 
 def train(stream,trafficStatistics,windowSize,bandCount):
-  flagArray = fromfile("op", dtype = int16, sep = '\n')
+  flagArray = fromfile("op_flag", dtype = int16, sep = '\n')
   windowCount = ceil(flagArray.size/windowSize)
   windowCount = int(windowCount)
   flagArray = flagArray.reshape(windowCount,windowSize)
@@ -38,25 +38,15 @@ def train(stream,trafficStatistics,windowSize,bandCount):
     for j in range(0,6):
       if stream.packetCounter[j] == 0:
         continue
-      stream.probabilityArray[j][stream.packetCounter[j]] += 1
-      savetxt("event", stream.probabilityArray)
-
+      stream.probabilityArray[j][stream.packetCounter[j]] += 1  
     stream.trainWindowCount += 1
     stream.packetCounter = zeros(6)
     print stream.trainWindowCount
   for i in range(0,6):
     band=determineOptimalBands(stream.probabilityArray[i],windowSize-1,bandCount)
     stream=updateProbabilities(stream,bandCount,band,i)
-    savetxt("band" + str(i), band)
+
 
 
 stream = TCPstream(10,5)
 train(stream,stream.packetCounter,stream.windowSize,stream.bandCount)
-#To find out the sum of the array : expected Probability=1
-arsum=0
-for row in range(len(stream.probabilityArray)):
-  for col in range(stream.windowSize):
-    arsum=arsum+(stream.probabilityArray[row][col])
-
-print arsum
-savetxt("capture", stream.probabilityArray)
