0340fb16a3713cac8524cf130004f844d5cd5cb4
mahidhar <mahidhar.c@gmail.com>
2014-04-12 14:12:17 +0530

    algo 2 begin

---
+++ algo2.py
@@ -0,0 +1,8 @@
+from algo3 import *
+
+def trainPhase(stream, packetCounter, windowSize, bandCount, errorProportion):
+  #update model probabilities
+  stream = train(stream, packetCounter, windowSize, bandCount)
+  #Determine threshold probability for the stream
+  stream.thresholdProbability=determineThresholdProbability(stream.windowSize,stream.packetCounter,stream.errorProportion)
+  return stream
