3a3972d4871f481cdfbc01bfc3b57e0b65d0a441
parthasarathy <parthasarathyalwar@gmail.com>
2014-04-13 09:18:38 +0530

    algo1 and front end begin

---
+++ algo1.py
@@ -0,0 +1,15 @@
+def deploy(stream, InputTraffic, awc):
+  #do the initialization
+
+a=0.0
+for i in range(windowCount):
+  for j in range(0,6)
+  p=determineProbability(windowCount, stream)
+  if ( p < stream.thresholdProbability):
+    a+=1
+  else:
+    a-=1
+  if a>=awc:
+    return attack
+
+
