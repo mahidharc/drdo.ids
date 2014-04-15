8dc2d61b2762425447175f64c89321fd6903ada7
mahidhar <mahidhar.c@gmail.com>
2014-04-15 17:58:16 +0530

    updated algo2

--- algo2.py
+++ algo2.py
@@ -2,10 +2,14 @@ from algo3 import *
 from algo7 import *
 from numpy import *
 
-def trainPhase(stream, packetCounter, windowSize, bandCount, errorProportion, groupCount):
-  #update model probabilities
-  flagArray = fromfile("op_flag", dtype = int16, sep = '\n')
+def trainPhase(windowSize, bandCount, groupCount):
+  stream = TCPstream(windowSize,bandCount)
+  flagArray = parse()
   stream = train(stream, windowSize, bandCount, flagArray)
   #Determine threshold probability for the stream
   stream.thresholdProbability = determineThresholdProbability(stream, groupCount, flagArray)
   return stream
+
+mainstream = TCPstream(100,40)
+mainstream=trainPhase(100,40,10)
+print mainstream.thresholdProbability
