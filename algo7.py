112c4c118419c9101e6b1cc034d1f6e1d81efb8d
mahidhar <mahidhar.c@gmail.com>
2014-04-15 18:02:50 +0530

    updated algo7

--- algo7.py
+++ algo7.py
@@ -2,6 +2,7 @@ from algo8 import *
 from algo3 import *
 from random import *
 from numpy import *
+from parser import *
 
 def determineThresholdProbability(stream, groupCount, flagArray):
   parray = zeros(groupCount)
@@ -12,7 +13,6 @@ def determineThresholdProbability(stream, groupCount, flagArray):
     for i in roll(flagArray,flagArray.size-initialValue)[:groupRange]:
       proportionArray=append(proportionArray,i)
     trainingArray = fromstring(flagArray.tostring().replace(proportionArray.tostring(), ""), dtype=flagArray.dtype)
-
     streamTemp = train(stream, stream.windowSize, stream.bandCount, trainingArray)
     windowCount = len(proportionArray)/stream.windowSize
     window = zeros(stream.windowSize)
@@ -20,13 +20,8 @@ def determineThresholdProbability(stream, groupCount, flagArray):
       rangeBegin = i*stream.windowSize
       for j in range(rangeBegin,rangeBegin+stream.windowSize):
         window[j%stream.windowSize] = proportionArray[j]
-      probability = determineProbability(window,streamTemp)
+      probability = determineProbability(window,streamTemp,flagArray)
       parray=append(parray,probability)
   parraySorted = sort(parray)
-  result = parraySorted[int(len(parray)*(stream.thresholdProbability/100))+1]
+  result = parraySorted[int(len(parray)*(stream.thresholdProbability))+1]
   return result
-
-stream = TCPstream(100,40)
-flagArray=fromfile("op_flag",dtype=int16,sep="\n")
-result=determineThresholdProbability(stream,10,flagArray)
-print result
--- algo8.py
+++ algo8.py
@@ -9,7 +9,7 @@ class TCPstream:
   trainWindowCount = 0
   packetCounter = zeros(6)
   probabilityArray = array([])
-  thresholdProbability = 1.0
+  thresholdProbability = 0.8
 
   def __init__(self,winSize,bandC):
     self.windowSize = winSize
@@ -17,12 +17,25 @@ class TCPstream:
     self.bandCount = bandC
 
 
-def determineProbability(window, stream):
+def determineProbability(window, stream, flagArray):
   probability = 1
   for i in range(len(window)):
-      None
+      if flagArray[i] == 1:
+        stream.packetCounter[0] += 1
+      elif flagArray[i] == 2:
+        stream.packetCounter[1] += 1
+      elif flagArray[i] == 3:
+        stream.packetCounter[2] += 1
+      elif flagArray[i] == 4:
+        stream.packetCounter[3] += 1
+      elif flagArray[i] == 5:
+        stream.packetCounter[4] += 1
+      else:
+        stream.packetCounter[5] += 1
     #TODO the if-elif-else flags statements from algorithm 3
   for i in range(0,6):
+    #print i,stream.packetCounter[i]
     probability *= stream.probabilityArray[i][stream.packetCounter[i]]
-
+  for i in range(6):
+    stream.packetCounter[i]=0
   return probability
