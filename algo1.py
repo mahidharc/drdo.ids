4a24223e8b21fcf5664d335c10e9594ed0466d10
mahidhar <mahidhar.c@gmail.com>
2014-04-15 17:57:52 +0530

    updated algo1

--- algo1.py
+++ algo1.py
@@ -1,13 +1,13 @@
 from algo2 import *
 
-def deploy(stream, InputTraffic, awc):
+def deploy(stream, inputTraffic, awc):
   a=0.0
   for i in range(stream.windowCount):
     for j in range(0,6):
-      p = determineProbability(stream.windowCount, stream)
+      p = determineProbability(stream.windowCount, stream, inputTraffic)
     if ( p < stream.thresholdProbability):
       a+=1
     else:
       a-=1
     if a>=awc:
-      return attack
+      return "attack"
