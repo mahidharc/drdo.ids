7fbab95306dee0f73fce6f6f8c7fed616094b9b8
mahidhar <mahidhar.c@gmail.com>
2014-04-15 18:01:54 +0530

    updated algo6

--- algo6.py
+++ algo6.py
@@ -5,15 +5,15 @@ def updateProbabilities(stream, bandCount, band, observableTypeIndex):
     stream.trainWindowCount += bandCount
     for j in range(0,bandCount):
       nelband = band[j][0]
-      for k in range(1,nelband+1):
+      for k in range(1,nelband):
         avgCount += stream.probabilityArray[observableTypeIndex][band[j][k]]
       avgCount += 1
       if nelband == 0:
         continue
       avgCount /= nelband
-      for k in range(1,nelband+1):
+      for k in range(1,nelband):
         stream.probabilityArray[observableTypeIndex][band[j][k]] = avgCount
-      for k in range(1,nelband+1):
+      for k in range(1,nelband):
         stream.probabilityArray[observableTypeIndex][band[j][k]] /= stream.trainWindowCount
       avgCount = 0
     return stream
