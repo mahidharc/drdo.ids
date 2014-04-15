13db0ee0b6aa27bd4b5735512920d2b2ec073b06
mahidhar <mahidhar.c@gmail.com>
2014-04-15 17:57:12 +0530

    updated parser

--- parser.py
+++ parser.py
@@ -1,3 +1,25 @@
 from numpy import *
 
-flagArray = fromfile("op_flag_hex", dtype = int16, sep = '\n')
+def parse():
+  flag = [line.strip() for line in open('op_flag')]
+  flags = []
+  for i in range(len(flag)):
+    flags.append(int(flag[i],0))
+  
+  flagArray = array([])
+  type1 = 4
+  
+  for i in flags:
+    if i&type1 == True:
+      flagArray = append(flagArray,1)
+    elif i == 2:
+      flagArray = append(flagArray,2)
+    elif i == 16:
+      flagArray = append(flagArray,3)
+    elif i == 17:
+      flagArray = append(flagArray,4)
+    elif i == 24:
+      flagArray = append(flagArray,5)
+    else:
+      flagArray = append(flagArray,6)
+  return flagArray
